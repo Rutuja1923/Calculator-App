51a7bd41a74e9b12601e8303e042101e23443c82
Rutuja1923 <rutujajirage19@gmail.com>
2024-08-17 12:45:19 +0530

    Added buttons

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,36 @@ function App() {
           <div className="result" />
         </div>
         <div className="buttons">
+          <div>
+            <button id="reset" ><i className="fa fa-refresh"></i></button>
+            <button id="backspace"><i className='fa fa-arrow-left'></i></button>
+            <button>%</button>
+            <button>÷</button>
+          </div>
+          <div>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>x</button>
+          </div>
+          <div>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>-</button>
+          </div>
+          <div>
+            <button>3</button>
+            <button>2</button>
+            <button>1</button>
+            <button>+</button>
+          </div>
+          <div>
+            <button>00</button>
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+          </div>
         </div>
       </div>
     </div>
